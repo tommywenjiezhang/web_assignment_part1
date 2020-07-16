7936218dbe9e13e971bb248315b752846ef224f2
tommywenjiezhang <tommyninteenies@gmail.com>
2020-07-16 12:29:52 -0400

    Fix added test suite

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -3,9 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
-    app.config.from_object('app.config.Config')
+    if test_config == None:
+        app.config.from_object('app.config.Config')
+    else:
+        app.config.update(test_config)
 
     db.init_app(app)
     with app.app_context():
--- app/test/test_cities.py
+++ app/test/test_cities.py
@@ -1,10 +1,48 @@
-import unittest
-import app.app.model
 
-class cities_test(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+from app import create_app, db
+import os
+import pytest
+import json
+
+TEST_DB = 'test.sqlite'
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture
+def client():
+    app = create_app({"TESTING": True,
+                      'SQLALCHEMY_DATABASE_URI': 'sqlite:///' + os.path.join(basedir, TEST_DB),
+                      'DEBUG': False
+                      })
+    with app.test_client() as client:
+        with app.app_context():
+            db.init_app(app)
+        yield client
+
+
+
+
+def test_get(client):
+    response = client.get("/cities",follow_redirects=True)
+    result =  {
+        "lng": 139.7514,
+        "capital": "primary",
+        "id": 1,
+        "population": 35676000,
+        "cityName": "Tokyo",
+        "lat": 35.685,
+        "abbreviation": "JPN",
+        "country": "Japan"
+    }
+    responseType = "type of data is"
+    assert response.status_code == 200
+    returnData = json.loads(response.data)
+    assert result in returnData
+
+def test_sample():
+    assert 2 == 2
+if __name__ == "__main__":
+    print('sqlite:///' + os.path.join(basedir, TEST_DB))
+
+
+
